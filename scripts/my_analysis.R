3c89afd4915a9d9137169240006d278feb417d72
haruosuz <haruosuz@yahoo.co.jp>
2015-12-03 19:14:58 +0900

    update README.md scripts/

--- scripts/my_analysis.R
+++ scripts/my_analysis.R
@@ -8,53 +8,58 @@ library(seqinr)
 
 # Reading sequence data into R
 #args <- commandArgs(trailingOnly = TRUE) # print(args)
-#ld <- read.fasta(file = paste0("data/",args[1],".fna") )
-ld <- read.fasta(file = "data/GCA_000027325.1_ASM2732v1_genomic.fna.gz") # mgen
-d <- ld[[1]]
+#lnt <- read.fasta(file = paste0("data/",args[1],".fna") )
+lnt <- read.fasta(file = "data/GCA_000027325.1_ASM2732v1_genomic.fna.gz")
+laa <- read.fasta(file = "data/GCA_000027325.1_ASM2732v1_protein.faa.gz", seqtype = c("AA") )
+
+cat("# How many sequences\n")
+length(lnt)
+length(laa)
+
+# The first element of the R list object `lnt` contains the DNA sequence.
+nt <- lnt[[1]]
+
+# Writing sequence data out as a FASTA file
+write.fasta(sequences=nt, names=getName(nt), file.out="analysis/sequence.fasta", nbchar=70)
 
 cat("# Length of a DNA sequence\n")
-length(d)
+length(nt)
 
 cat("# Base composition of a DNA sequence\n")
-( x <- table(d) )
+( x <- table(nt) )
 x / sum(x)
 
 cat("# GC Content of DNA\n")
 (x["g"] + x["c"]) / sum(x)
 #GC(s2c("acgt")) # 0.5
-GC(d)
+GC(nt)
 
 # DNA words
 cat("# Dinucleotide counts\n")
 #count(s2c("atg"), 2) # "at" and "tg"
-count(d, 2)
-
+count(nt, 2)
 # http://www.ncbi.nlm.nih.gov/pmc/articles/PMC17754/figure/F1/
-cat("# Dinucleotide relative abundances (Rho)\n")
-rho(d, 2)
+cat("# Dinucleotide relative abundances (Rho)\n"); rho(nt, 2)
 
 cat("# Local variation in GC content\n")
-GC(d[1:100000])
-GC(d[100001:200000])
-GC(d[200001:300000])
-GC(d[300001:400000])
-GC(d[400001:500000])
-GC(d[500001:580076])
+GC(nt[1:100000])
+GC(nt[100001:200000])
+GC(nt[200001:300000])
+GC(nt[300001:400000])
+GC(nt[400001:500000])
+GC(nt[500001:580076])
 
-cat("# A sliding window analysis of GC content\n")
 library(zoo)
-rollapply(data = d, width = 100000, by = 100000, FUN = GC)
+cat("# A sliding window analysis of GC content\n")
+rollapply(data = nt, width = 100000, by = 100000, FUN = GC)
 
 # A sliding window plot of GC content
-windowsize <- 10000
-x <- seq(from = 1, to = length(d)-windowsize, by = windowsize)
-y <- rollapply(data = d, width = windowsize, by = windowsize, FUN = GC)
 pdf(file="analysis/plot.pdf")
+windowsize <- 10000
+x <- seq(from = 1, to = length(nt)-windowsize, by = windowsize)
+y <- rollapply(data = nt, width = windowsize, by = windowsize, FUN = GC)
 plot(x, y, type="l", xlab="Position (bp)", ylab="GC content")
 dev.off()
-
-# Writing sequence data out as a FASTA file
-write.fasta(sequences=d, names=getAnnot(d), file.out="analysis/sequence.fna", nbchar=70)
 
 # 
 sessionInfo()

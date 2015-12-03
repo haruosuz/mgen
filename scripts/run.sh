600f58532c92d0df0159d332cc28291a1135db07
haruosuz <haruosuz@yahoo.co.jp>
2015-12-03 19:52:45 +0900

    update README.md scripts/

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 mkdir -p ./{data,analysis/results-$(date +%F)}
 
 # Downloading data
+# In the assembly summary file ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Mycoplasma_genitalium/assembly_summary.txt # The field named "ftp_path" provides the path to the FTP directory containing the data for each assembly.
 
 URL=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA_000027325.1_ASM2732v1/
 wget -P data/ $URL/{README.txt,md5checksums.txt,*.fna.gz,*.faa.gz}
